c4c5cc09ac2adba63e3941f4117b58924beb4207
jcervantes2113 <josecervantes678@gmail.com>
2016-09-21 19:35:25 -0700

    CSC 17A

--- Homework/Cervantes_Jose_Review_48096/Gaddis_Ch_4_Prob_7_Pog_Chall/main.cpp
+++ Homework/Cervantes_Jose_Review_48096/Gaddis_Ch_4_Prob_7_Pog_Chall/main.cpp
@@ -11,7 +11,7 @@
  * Created on September 21, 2016, 12:25 PM
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -19,7 +19,42 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    
+    int time(0);
+    int convsn(0);
+    int seconds(0);
+    int minuetes(0);
+    int days(0);
+    
+    
+    cout<<"This program converts seconds into its simplified time frame."<<endl;
+    
+    cout<<"Please enter the time you wish to convert:";
+    cin>>time;
+    
+    if(time<3600){
+        convsn=time/60;
+        seconds=time - (convsn*60);
+        cout<<convsn<<" Minutes and "<<seconds<<" seconds is the converted time"<<endl;
+        
+    }else if(time<86400 && time>=3600){
+        convsn=time/3600;
+        minuetes=(time-(convsn*3600))/60;
+        seconds= (time-(convsn*3600))%60;
+        
+        cout<<convsn<<" hour and "<<minuetes<<" with "<<seconds<<" seconds "
+              "minutes is the converted time"<<endl;
+    }else{
+        
+        days=time/86400;
+        convsn=((time-(days*86400))/3600);
+        minuetes=((time-(days*86400))-(convsn*3600))/60;
+        seconds= (time-(convsn*86400))%60;
+        
+        cout<<days<<" day(s) "<<convsn<<" hour and "<<minuetes<<" with "<<seconds<<" seconds "
+              "minutes is the converted time"<<endl;
+    }
+    
     return 0;
 }
 
